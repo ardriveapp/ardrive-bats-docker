095b501f3e1240b0a78df9467fafb2724f136940
agsuy <agustin@ardrive.io>
2022-05-18 00:18:48 -0300

    feature(arlocal):Automatically sets arlocal env

--- arlocal/index.mjs
+++ arlocal/index.mjs
@@ -1,35 +1,61 @@
 import * as fs from 'fs'
-import Sweets from 'arlocal-sweets';
-import Arweave from 'arweave';
+import * as path from 'path'
+import Sweets from 'arlocal-sweets'
+import Arweave from 'arweave'
 
 const walletFolder = "./wallets/"
+const envFilePath = "./bats-variables.env"
 
-const hostsFile = fs.readFileSync('/etc/hosts', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
+const getArlocalHost = async () => {
+    try {
+        const data = await fs.promises.readFile('/etc/hosts', 'utf8')
+        //Get last line from file.
+        let lastLine = data.trim().split('\n').slice(-1)[0]
+        //Get host from last line.
+        let host = lastLine.match(/^(\S+)\s(.*)/).slice(1)[0]
+        //Transform using subnet
+        let arlocalGW = host.match(/.*\./) + '1'
+        return arlocalGW
     }
-})
+    catch (err) {
+        console.error(err)
+    }
+}
 
 const storeWallet = (wallet, name) => {
     let jsonContent = JSON.stringify(wallet)
     let jsonWalletName = name + ".json"
-    fs.writeFileSync(`${walletFolder}/${jsonWalletName}`, jsonContent, 'utf8', function (err) {
+    fs.writeFile(`${walletFolder}/${jsonWalletName}`, jsonContent, 'utf8', function (err) {
         if (err) {
-            console.error("An error occured while writing JSON wallet to a File.")
+            console.error("An error occurred while writing JSON wallet to a File.")
             return console.log(err)
         }
     })
 }
-//Get last line from file.
-let lastLine = hostsFile.trim().split('\n').slice(-1)[0]
-//Get host from last line.
-let host = lastLine.match(/^(\S+)\s(.*)/).slice(1)[0]
-//Tansform into Subnet
-let arlocalGW = host.slice(0, -1) + '1'
+
+const updateEnvFile = (matchString, filePath, replaceString) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        let regex = new RegExp('^.*' + matchString + '.*$', 'm')
+        let formatted = data.replace(regex, replaceString)
+        fs.writeFile(filePath, formatted, 'utf8', function (err) {
+            if (err) return console.log(err)
+        })
+    })
+}
+
+//Get Arlocal Host
+const arlocalHost = await getArlocalHost()
+//Generate ENV variable
+const envArlocalGW = "ARLOCAL_GW=" + "'" + arlocalHost + "'"
+//Update ENV file
+updateEnvFile('ARLOCAL_GW=', envFilePath, envArlocalGW)
 
 const arweave = new Arweave({
-    host: arlocalGW,
+    host: arlocalHost,
     port: 1984,
     protocol: 'http'
 });
@@ -43,10 +69,13 @@ if (await sweets.isTestNetwork()) {
     await sweets.fundWallet(100e12)
     console.log("Wallet funded successfully")
     storeWallet(wallet, address)
+    let jsonWalletPath = path.resolve(walletFolder + address + ".json")
+    //Update ENV file
+    updateEnvFile('ARLOCAL_WALLET=', envFilePath, 'ARLOCAL_WALLET=' + jsonWalletPath)
     console.log("Wallet file has been saved.")
     console.log("Wallet address:", address)
-
 } else {
     console.error("Cannot reach Arlocal")
 }
-//missing setup ENV var and trigger
+
+//TODO: Trigger to load this .env and documentation
